f5424f049a58a3371a9c499e0704693ae6183e20
AntiAegis <thuypn9a4@gmail.com>
2019-08-30 16:00:56 +0700

    Update

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="convcrf",
+    version="0.0.1",
+    author="thuyngch",
+    author_email="thuypn9a4@gmail.com",
+    description="A fork from https://github.com/MarvinTeichmann/ConvCRF",
+    long_description="long_description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/thuyngch/ConvCRF",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
